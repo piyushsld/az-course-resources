0fc73a26fd723d4163626a0a2dbbae9336821578
Piyush Sharma <piyush2192@gmail.com>
2026-04-19 16:30:19 +0530

    add module structure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-demo-${var.environment}"
+  name     = "rg-terra-${var.environment}"
   location = var.location
 
   tags = {
@@ -7,9 +7,21 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "vnet_terra" {
   name                = "vnet-${var.environment}"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = [var.address_space]
+}
+
+resource "azurerm_resource_group" "rg_new" {
+  for_each = var.resource_groups
+  
+  name     = "rg-${each.key}-${var.environment}"
+  location = each.value
+  
+  tags = {
+    Environment = var.environment
+    Purpose     = each.key
+  }
 }
