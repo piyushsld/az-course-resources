5f3261e540e200ac9199ebf8b705c84c794cb6f5
Piyush Sharma <piyush2192@gmail.com>
2026-05-23 17:41:30 +0530

    add workflows

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -42,3 +42,18 @@ variable "subnet_name" {
 variable "subnet_address_prefix" {
   type = string
 }
+
+variable "client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "tenant_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "subscription_id" {
+  type      = string
+  sensitive = true
+}
