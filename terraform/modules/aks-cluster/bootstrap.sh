6ac9f3c7e275f09e190dd86bf9eec4b892f4a411
Piyush Sharma <piyush2192@gmail.com>
2026-05-30 14:02:57 +0530

    bootstrap updates

--- terraform/modules/aks-cluster/bootstrap.sh
+++ terraform/modules/aks-cluster/bootstrap.sh
@@ -2,10 +2,35 @@
 
 set -euxo pipefail
 
+sudo cd /home/azureuser
+
 echo "Bootstrap started: $(date)"
 
-apt-get update
-mkdir actions-runner && cd actions-runner
+
+sudo curl -fsSL 'https://azurecliprod.blob.core.windows.net/$root/deb_install.sh' | sudo bash
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update && sudo apt install unzip tar
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo mkdir actions-runner && cd actions-runner
 
 curl -o actions-runner-linux-x64-2.334.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.334.0/actions-runner-linux-x64-2.334.0.tar.gz
 echo "048024cd2c848eb6f14d5646d56c13a4def2ae7ee3ad12122bee960c56f3d271  actions-runner-linux-x64-2.334.0.tar.gz" | shasum -a 256 -c
